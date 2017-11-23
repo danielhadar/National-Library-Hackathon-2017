370ee08d0300e450ad1b675fbb9bfb44aeb6fef5
Daniel Hadar <danielhadar@gmail.com>
2017-11-23 19:15:10 +0200

    update main scheme, add process_image

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import sys
 import scipy.misc
 from urllib.request import urlopen
 
-from PIL import Image
-import requests
-from io import BytesIO
-
 import colorize
 import impr_tools
 
 import warnings
 warnings.filterwarnings("ignore")
 
-
 def image_from_url(url):
 	with urlopen(url) as file:
-		img = scipy.misc.imread(file, mode='RGB') / 255.
-
-	# response = requests.get(url)
-	# img = Image.open(BytesIO(response.content))
-	return img
+		return scipy.misc.imread(file, mode='RGB') / 255.
 
 
 def boolean_from_char(char):
@@ -28,19 +19,14 @@ def boolean_from_char(char):
 	return True
 
 
-usage = 'Usage: python main.py <file_url> <[y]/n: colorize> <[y]/n: histogram equalization> <[y]/n: adjust saturation>'
-
-if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print(usage)
-		exit()
-
+def process_image(image_url, is_url, should_colorize, should_equalize_hist, should_adjust_saturation):
 	# Download image
-	image_url = sys.argv[1]
-	img = image_from_url(image_url)
+	if is_url:
+		img = image_from_url(image_url)
+	else:
+		img = scipy.misc.imread(image_url, mode='RGB') / 255.
 
 	# Colorize
-	should_colorize = True if len(sys.argv) <= 2 else boolean_from_char(sys.argv[2])
 	if should_colorize:
 		print("#-- Running Colorizing --#\n")
 		img = colorize.colorize_image(img)
@@ -48,7 +34,6 @@ if __name__ == '__main__':
 		print("!-- Skipping Colorizing --!\n")
 
 	# Histogram Equalization
-	should_equalize_hist = True if len(sys.argv) <= 3 else boolean_from_char(sys.argv[3])
 	if should_equalize_hist:
 		print("#-- Running Histogram Equalization --#\n")
 		img = impr_tools.histogram_equalization(img)
@@ -56,7 +41,6 @@ if __name__ == '__main__':
 		print("!-- Skipping Histogram Equalization --!\n")
 
 	# Adjust Saturation
-	should_adjust_saturation = True if len(sys.argv) <= 4 else boolean_from_char(sys.argv[4])
 	if should_adjust_saturation:
 		print("#-- Running Adjust Saturation --#\n")
 		img = impr_tools.adjust_saturation(img, saturation_factor=1.15)
@@ -64,5 +48,20 @@ if __name__ == '__main__':
 		print("!-- Skipping Adjust Saturation --!\n")
 
 	# Done
-	scipy.misc.imsave('/home/odelya/public_html‏/final_image.png', img)
+	scipy.misc.imsave('./var/www/html/final_image.png', img)
 	print('----> Done! <----\n')
+
+
+usage = 'Usage: python main.py <file_url> <[url]/file> <[y]/n: colorize> <[y]/n: histogram equalization> <[y]/n: adjust saturation>'
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print(usage)
+		exit()
+
+	image_url = sys.argv[1]
+	is_url = len(sys.argv) <= 2 or sys.argv[2] == 'url'
+	should_colorize = True if len(sys.argv) <= 3 else boolean_from_char(sys.argv[3])
+	should_equalize_hist = True if len(sys.argv) <= 4 else boolean_from_char(sys.argv[4])
+	should_adjust_saturation = True if len(sys.argv) <= 5 else boolean_from_char(sys.argv[5])
+	process_image(image_url, is_url, should_colorize, should_equalize_hist, should_adjust_saturation)
