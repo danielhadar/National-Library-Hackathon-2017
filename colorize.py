7d67c0ab4b9886f388033672c76905e5e9e18a40
Daniel Hadar <danielhadar@gmail.com>
2017-11-23 17:55:07 +0200

    add main

--- colorize.py
+++ colorize.py
@@ -10,20 +10,16 @@ import skimage.io
 import scipy.misc
 
 MIN_AXIS_SIZE = 360
+TEMP_IMG_NAME = 'temp'
 
-def colorize_image(fn):
-    input_image = scipy.misc.imread(fn, mode='RGB') / 255.
-
+def colorize_image(input_image):
     small_size = np.array(input_image.shape[:2]) * (float(MIN_AXIS_SIZE) / min(input_image.shape[:2]))
     small_size = np.round(small_size).astype(int)
     input_image_downsampled = skimage.transform.resize(input_image, small_size, preserve_range=True,
                                                        mode='reflect')
 
-    ext = os.path.splitext(fn)[1]
-
-    small_fn = fn.replace(ext, '.small' + ext)
-    small_colored_fn = fn.replace(ext, '.small.color' + ext)
-    out_fn = fn.replace(ext, '.color' + ext)
+    small_fn = TEMP_IMG_NAME+'.small.png'
+    small_colored_fn = TEMP_IMG_NAME+'.small.color.png'
 
     scipy.misc.imsave(small_fn, input_image_downsampled)
     os.system('th colorize.lua %s %s colornet.t7' % (small_fn, small_colored_fn))
@@ -39,9 +35,9 @@ def colorize_image(fn):
     combined_lab = np.concatenate([orig_lum[:, :, 0:1], upscaled_chroma], axis=-1)
     out_image = skimage.color.yuv2rgb(combined_lab).clip(0., 1.)
 
-    scipy.misc.imsave(out_fn, out_image)
     os.unlink(small_fn)
     os.unlink(small_colored_fn)
+    return out_image
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,25 +1,68 @@
 import sys
+import scipy.misc
+from urllib.request import urlopen
+
 from PIL import Image
 import requests
 from io import BytesIO
 
+import colorize
+import impr_tools
 
-import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def image_from_url(url):
-	response = requests.get(url)
-	img = Image.open(BytesIO(response.content))
+	with urlopen(url) as file:
+		img = scipy.misc.imread(file, mode='RGB') / 255.
+
+	# response = requests.get(url)
+	# img = Image.open(BytesIO(response.content))
 	return img
 
 
+def boolean_from_char(char):
+	if char == 'n':
+		return False
+	return True
 
 
-# Usage: python main.py <file_url> <[y]/n: colorize> <[y]/n: super-resolution>
+usage = 'Usage: python main.py <file_url> <[y]/n: colorize> <[y]/n: histogram equalization> <[y]/n: adjust saturation>'
+
 if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print(usage)
+		exit()
+
 	# Download image
 	image_url = sys.argv[1]
 	img = image_from_url(image_url)
 
 	# Colorize
-	if 
+	should_colorize = True if len(sys.argv) <= 2 else sys.argv[2]
+	if should_colorize:
+		print("#-- Running Colorizing --#\n")
+		img = colorize.colorize_image(img)
+	else:
+		print("!-- Skipping Colorizing --!\n")
+
+	# Histogram Equalization
+	should_equalize_hist = True if len(sys.argv) <= 3 else sys.argv[3]
+	if should_equalize_hist:
+		print("#-- Running Histogram Equalization --#\n")
+		img = impr_tools.histogram_equalization(img)
+	else:
+		print("!-- Skipping Histogram Equalization --!\n")
+
+	# Adjust Saturation
+	should_adjust_saturation = True if len(sys.argv) <= 4 else sys.argv[4]
+	if should_adjust_saturation:
+		print("#-- Running Adjust Saturation --#\n")
+		img = impr_tools.adjust_saturation(img, saturation_factor=1.15)
+	else:
+		print("!-- Skipping Adjust Saturation --!\n")
+
+	# Done
+	scipy.misc.imsave('./final_image.png', img)
+	print('︵ \(°□°)/ ︵ ----> Done!\n')
