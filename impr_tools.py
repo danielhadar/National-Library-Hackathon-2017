8d93cb9e2868b4520d7eab5f473f3c7c45343ecd
Daniel Hadar <danielhadar@gmail.com>
2017-11-23 14:01:51 +0200

    add saturation

--- impr_tools.py
+++ impr_tools.py
@@ -1,10 +1,10 @@
 import sys
 from skimage import exposure
+from skimage import color
+
 import scipy.misc
 
-
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
 def histogram_equalization(img):
@@ -17,11 +17,19 @@ def adaptive_histogram_equalization(img):
 	return img_adapteq
 
 
+def adjust_saturation(img, saturation_factor):
+	hsv = color.rgb2hsv(img)
+	hsv[:,:,1] *= saturation_factor
+	return color.hsv2rgb(hsv)
+
+
+# used for eyeball testing
 if __name__ == '__main__':
 	filename = sys.argv[1]
 	input_image = scipy.misc.imread(filename, mode='RGB') / 255.
 
-	output_image = histogram_equalization(input_image)
+	# output_image = histogram_equalization(input_image)
+	output_image = adjust_saturation(input_image, 1)
 
 	fig = plt.figure()
 	fig.add_subplot(1, 2, 1)
